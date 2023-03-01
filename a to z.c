b91504a6ceec02ca7cceb1d658fca29ea61ed932
purvakhant421 <115995434+purvakhant421@users.noreply.github.com>
2023-03-01 14:41:06 +0530

    Add files via upload

---
+++ a to z.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()  
+{  
+    char ch = 'a';  
+  
+    printf("Lowercase English Alphabets:\n");  
+    while(ch <= 'z')  
+    {  
+        printf("%c ", ch);  
+        ch++;  
+    }  
+    printf("\n");  
+  
+    return 0;  
+}  
